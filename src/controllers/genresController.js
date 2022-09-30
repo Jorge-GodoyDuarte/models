bbf3af722799d7bc33d120a85a9f18c5e1b0deda
Jorge-GodoyDuarte <hawkaa666@gmail.com>
2022-09-30 19:09:59 +0200

    tp models

---
+++ src/controllers/genresController.js
@@ -0,0 +1,21 @@
+const db = require('../database/models')
+
+module.exports = {
+    list:(req,res)=>{
+        db.Genre.findAll()
+        .then(genres=> res.render('genresList',{genres}))
+        .catch(error => console.log(error))
+
+    },
+    new:(req,res)=>{
+        
+    },
+    recomended :(req,res)=>{
+
+    },
+    detail:(req,res)=>{
+        db.Genre.findByPk(req.params.id)
+            .then(genre=>res.render('genresDetail',{genre}))
+            .catch(error=>console.log(error))
+    }
+}
